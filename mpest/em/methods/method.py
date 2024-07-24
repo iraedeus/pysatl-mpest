40435ccf049c9f21db4b36d07c0219b7e0733fe0
iraedeus <dtotjmyanin@mail.ru>
2024-07-24 18:44:13 +0300

    style: pylint fix

--- mpest/em/methods/method.py
+++ mpest/em/methods/method.py
@@ -1,3 +1,5 @@
+"""Module that represents method class"""
+
 from mpest.em.methods.abstract_steps import AExpectation, AMaximization
 from mpest.mixture_distribution import MixtureDistribution
 from mpest.problem import Problem
@@ -5,10 +7,27 @@ from mpest.utils import ResultWithError
 
 
 class Method:
+    """
+    Class that performs E and M steps.
+    """
+
     def __init__(self, e_step: AExpectation, m_step: AMaximization):
+        """
+        Object constructor.
+
+        :param e_step: AExpectation object which performs E step
+        :param e_step: AMaximization object which performs M step
+        """
+
         self.e_step = e_step
         self.m_step = m_step
 
     def step(self, problem: Problem) -> ResultWithError[MixtureDistribution]:
+        """
+        Function that performs E and M steps
+
+        :param problem: Object of Problem class which contains samples and mixture.
+        """
+
         e_result = self.e_step.step(problem)
         return self.m_step.step(e_result)
